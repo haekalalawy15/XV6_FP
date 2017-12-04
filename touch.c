5df8fe4d27cbed894f8acbab97b406ce64ff4486
khairahmard <khairunnisamardiyani@gmail.com>
2017-12-04 18:08:48 +0700

    insyaAllah bener

--- touch.c
+++ touch.c
@@ -6,11 +6,16 @@
 int main(int argc, char *argv[])
 {
     int i;
+    
+    if(argc<2){
+         printf (2,"need 2 arguments\n");
+	 exit();
+    }
 
     for (i=1;i<argc;i++){
          int fd;
          fd=open(argv[i], O_CREATE|O_RDWR);
     	 close(fd);
+	 exit();
     }
-    exit();
 }
