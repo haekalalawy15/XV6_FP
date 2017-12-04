1cf61bbc36c6778be7f2e7044467d3e7354b4a21
khairahmard <khairunnisamardiyani@gmail.com>
2017-12-04 18:09:48 +0700

    masih ngaco caranya

---
+++ rmbaru.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for(i=1;i<argc;i++){
+        int fd;
+	fd=open(argv[i], O_CREATE|O_RDWR);
+	unlink(argv[i]);
+	close(fd);
+    }
+    exit();
+}
