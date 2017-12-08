f1fa0653e1d6622310655e3df7d75f5bab29c760
putrinapr <32903499+putrinapr@users.noreply.github.com>
2017-12-08 12:57:39 +0700

    cd

---
+++ cd.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		printf(1, "Too many/few arguments, use: cd <directory>.\n");
+		exit();
+	}
+	
+	if(chdir(argv[1]) == 0)
+	{
+		printf(1, "Directory changed to %s.\n", argv[1]);
+		exit();
+	}
+	else
+	{
+		printf(1, "Could not change directory.\n");
+    exit();
+	}
+}
